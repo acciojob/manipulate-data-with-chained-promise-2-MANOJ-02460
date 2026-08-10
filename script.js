075f457b2fd1e21a1f670fb3be0db58edf39f90a
MANOJ-02460 <131439674+MANOJ-02460@users.noreply.github.com>
2026-08-10 22:08:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+let outputElement = document.getElementById("output");
+
+let array = [1,2,3,4];
+
+function transformationOfArray(){
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+		resolve(array);
+	},3000)
+	})
+
+	
+}
+
+transformationOfArray()
+	.then((arr)=>{
+	const evenNumbers = arr.filter((num)=>{
+		return num % 2 === 0
+	})
+
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			outputElement.textContent = evenNumbers;
+			resolve(evenNumbers)
+		},1000)
+	})
+})
+.then((evenNumbers)=>{
+	const multipliedNumbers = evenNumbers.map((items)=>{
+		return items*2
+	})
+
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			outputElement.textContent = multipliedNumbers
+		},3000)
+	})
+})
